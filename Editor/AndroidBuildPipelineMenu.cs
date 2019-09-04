03adddb333ff0e66d460dad23cead9268e06e899
Thanut Panichyotai <luvikung@gmail.com>
2019-09-04 11:15:25 +0700

    new version 1.0.3 change menu into settings. fix bugs that set build location isn't working properly.

--- Editor/AndroidBuildPipelineMenu.cs
+++ Editor/AndroidBuildPipelineMenu.cs
@@ -15,17 +15,12 @@ namespace LuviKunG.BuildPipeline.Android
         {
             if (UnityEditor.BuildPipeline.isBuildingPlayer)
                 return;
-            settings = settings ?? new AndroidBuildPipelineSettings();
-            string loadedBuildPath = settings.buildPath;
-            string directoryPath;
-            if (string.IsNullOrEmpty(loadedBuildPath))
-            {
-                directoryPath = EditorUtility.SaveFolderPanel("Choose Location of Build Game", loadedBuildPath, null);
-                if (string.IsNullOrEmpty(directoryPath))
-                    return;
-            }
+            settings = AndroidBuildPipelineSettings.Instance;
+            string path;
+            if (string.IsNullOrEmpty(settings.buildPath))
+                path = OpenBuildSavePanel(settings.buildPath);
             else
-                directoryPath = loadedBuildPath;
+                path = settings.buildPath;
             var scenes = new List<EditorBuildSettingsScene>(EditorBuildSettings.scenes);
             for (int i = 0; i < scenes.Count; i++)
                 if (!scenes[i].enabled)
@@ -46,13 +41,13 @@ namespace LuviKunG.BuildPipeline.Android
                 PlayerSettings.Android.keyaliasPass = settings.keyaliasPass;
             }
             string fileName = settings.GetFileName();
-            string buildPath = Path.Combine(directoryPath, fileName);
+            string buildPath = Path.Combine(path, fileName);
             BuildReport report = UnityEditor.BuildPipeline.BuildPlayer(scenes.ToArray(), buildPath, BuildTarget.Android, BuildOptions.None);
             BuildSummary summary = report.summary;
             if (summary.result == BuildResult.Succeeded)
             {
                 Debug.Log($"Build succeeded at '{buildPath}' using {summary.totalTime.TotalSeconds.ToString("N2")} seconds with size of {summary.totalSize} bytes.");
-                Application.OpenURL(directoryPath);
+                Application.OpenURL(path);
             }
             if (summary.result == BuildResult.Failed)
             {
@@ -60,24 +55,22 @@ namespace LuviKunG.BuildPipeline.Android
             }
         }
 
+#if BUILD_PIPELINE_ANDROID_OLD_MENU
         [MenuItem("Build/Settings/Android/Set Build Directory...", false, 1)]
         public static void SetDirectory()
         {
             if (UnityEditor.BuildPipeline.isBuildingPlayer)
                 return;
-            settings = settings ?? new AndroidBuildPipelineSettings();
-            string loadedBuildPath = settings.buildPath;
-            string newPath = EditorUtility.SaveFolderPanel("Choose Location of Build Game", loadedBuildPath, null);
-            if (string.IsNullOrEmpty(newPath))
-                return;
-            EditorUserBuildSettings.SetBuildLocation(BuildTarget.Android, newPath);
-            Debug.Log($"Build directory has been set to: {newPath}");
+            settings = AndroidBuildPipelineSettings.Instance;
+            string newPath = OpenBuildSavePanel(settings.buildPath);
+            if (!string.IsNullOrEmpty(newPath))
+                EditorUtility.DisplayDialog("Android Build Pipeline", $"Build directory has been set to: {newPath}", "Okay");
         }
 
         [MenuItem("Build/Settings/Android/Open Directory...", true, 2)]
         public static bool OpenDirectoryValidate()
         {
-            settings = settings ?? new AndroidBuildPipelineSettings();
+            settings = AndroidBuildPipelineSettings.Instance;
             string loadedBuildPath = settings.buildPath;
             return !string.IsNullOrEmpty(loadedBuildPath);
         }
@@ -87,18 +80,33 @@ namespace LuviKunG.BuildPipeline.Android
         {
             if (UnityEditor.BuildPipeline.isBuildingPlayer)
                 return;
-            settings = settings ?? new AndroidBuildPipelineSettings();
+            settings = AndroidBuildPipelineSettings.Instance;
             string loadedBuildPath = settings.buildPath;
             if (string.IsNullOrEmpty(loadedBuildPath))
                 return;
             Application.OpenURL(loadedBuildPath);
         }
 
-
         [MenuItem("Build/Settings/Android/Open Build Settings...", false, 3)]
         public static void OpenBuildSetting()
         {
             AndroidBuildPipelineSettingsWindow.OpenWindow();
+        }
+#else
+        [MenuItem("Build/Settings/Android", false, 0)]
+        public static void OpenBuildSetting()
+        {
+            AndroidBuildPipelineSettingsWindow.OpenWindow();
+        }
+#endif
+        private static string OpenBuildSavePanel(string path)
+        {
+            string newPath = EditorUtility.SaveFolderPanel("Choose Location of Build Game", path, null);
+            if (string.IsNullOrEmpty(newPath))
+                return null;
+            settings.buildPath = newPath;
+            settings.Save();
+            return newPath;
         }
     }
 }
